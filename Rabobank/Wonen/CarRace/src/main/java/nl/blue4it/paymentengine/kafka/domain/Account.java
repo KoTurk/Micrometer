05e893b17f0e609c14ff785db7791ce9c5c0bf06
Turk <ko.turk@rabobank.nl>
2022-06-13 23:12:44 +0200

    Renamed Wonen to ChapterWonen

--- Rabobank/Wonen/CarRace/src/main/java/nl/blue4it/paymentengine/kafka/domain/Account.java
+++
@@ -1,4 +0,0 @@
-package nl.blue4it.paymentengine.kafka.domain;
-
-public class Account {
-}
--- Rabobank/Wonen/CarRace/src/main/java/nl/blue4it/paymentengine/rest/exception/CrashException.java
+++
@@ -1,6 +0,0 @@
-package nl.blue4it.paymentengine.rest.exception;
-
-public class CrashException extends RuntimeException {
-    public CrashException(String oh_no) {
-    }
-}
